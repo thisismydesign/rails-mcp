6439921549fc1f1b5f59b0ec310b326a1dc72b83
Csaba Apagyi <csaba.apagyi@gmail.com>
2025-11-28 00:35:32 +0100

    Allow browser-based access to mcp

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,17 @@
 
 class ApplicationController < ActionController::API
   def mcp2
+    # Config for browser-based inspector:
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = '*'
+
+    # Handle CORS preflight
+    if request.method == "OPTIONS"
+      head :ok
+      return
+    end
+
     server = MCP::Server.new(
       name: "rails-mcp",
       version: "1.0.0",
